ec55a9d218f21701d6a15b3e6feb63079444f749
nagaremono <nagaremono@tutanota.com>
2021-08-01 21:23:16 +0800

    add winston logger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import app from './app';
 import CONFIG from './config/config';
+import logger from './config/logger';
 
 function main() {
   const server = app.listen(CONFIG.PORT, () => {
-    console.log('server started on *:4000');
+    logger.info(`server started on *:${CONFIG.PORT}`);
   });
 
   process.on('SIGTERM', () => {
     server.close(() => {
-      console.log('Server terminated');
+      logger.info('Server terminated');
     });
   });
 }
